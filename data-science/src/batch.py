23b1bfc20ef54b39a4c36e4ec33f3ba0e1d36e31
cindyweng <weng.cindy@gmail.com>
2022-03-23 17:27:51 +0000

    removed dummy files and added actual files for training pipeline

---
+++ data-science/src/batch.py
@@ -0,0 +1,51 @@
+  
+import os
+import glob
+import json
+import argparse
+import numpy as np
+import pandas as pd
+import joblib
+from azureml.core.model import Model
+
+model = None
+
+def init():
+    global model
+    print("Started batch scoring by running init()")
+    
+    parser = argparse.ArgumentParser('batch_scoring')
+    parser.add_argument('--model_name', type=str, help='Model to use for batch scoring')
+    args, _ = parser.parse_known_args()
+    
+    model_path = Model.get_model_path(args.model_name)
+    print(f"Model path: {model_path}")
+    model = joblib.load(model_path)
+
+def run(file_list):
+    print(f"Files to process: {file_list}")
+    try:
+        results = pd.DataFrame(columns=["Sno", "ProbaGoodCredit", "ProbaBadCredit"])
+        for filename in file_list:
+            df = pd.read_csv(filename)
+            sno = df["Sno"]
+            df = df.drop("Sno", axis=1)
+            proba = model.predict_proba(df)
+            proba = pd.DataFrame(data=proba, columns=["ProbaGoodCredit", "ProbaBadCredit"])
+            result = pd.concat([sno, proba], axis=1)
+            results = results.append(result)
+            print(f"Batch scored: {filename}")
+        return results
+    except Exception as e:
+        error = str(e)
+        return error
+
+def test():
+    global model
+    model = joblib.load("./outputs/model.pkl")
+    files = ['data/credit_batch.csv']
+    result = run(files)
+    print(result)
+
+if __name__ == "__main__":
+    test()
