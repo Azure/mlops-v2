4a5a4f1377da9b15d4e240e015f85e8ba07fb3f5
setuc <setuc@DESKTOP-U5GVRDS.localdomain>
2022-04-13 22:36:14 +0530

    Replace Infrastructure with submodule

--- infrastructure/terraform/modules/container-registry/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "id" {
-  value = azurerm_container_registry.adl_cr.id
-}
--- infrastructure/terraform/modules/key-vault/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = azurerm_key_vault.adl_kv.id
-}
-
-output "name" {
-  value = azurerm_key_vault.adl_kv.name
-}
