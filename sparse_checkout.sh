45566e4a8b567fb75fa2bb56d1719038345406b0
Ritaja Sengupta <ritaja.sengupta@gmail.com>
2023-03-17 12:19:43 +0100

    Update sparse_checkout.sh
    remove local references.

--- sparse_checkout.sh
+++ sparse_checkout.sh
@@ -3,9 +3,9 @@ project_type=classical      #options: classical / cv / nlp
 mlops_version=aml-cli-v2   #options: aml-cli-v2 / python-sdk-v1 / python-sdk-v2 / rai-aml-cli-v2
 orchestration=azure-devops #options: github-actions / azure-devops
 git_folder_location='<local path>'   #replace with the local root folder location where you want to create the project folder
-project_name=ml-classical-fs   #replace with your project name
-github_org_name=ritaja   #replace with your github org name
-project_template_github_url=https://github.com/ritaja/mlops-project-template  #replace with the url for the project template for your organization created in step 2.2, or leave for demo purposes
+project_name=Mlops-Test   #replace with your project name
+github_org_name=orgname   #replace with your github org name
+project_template_github_url=https://github.com/azure/mlops-project-template   #replace with the url for the project template for your organization created in step 2.2, or leave for demo purposes
 
 cd $git_folder_location
 
