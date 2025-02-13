022bd8f4c43df5eaf057f4b35da051819342c5fc
jalalkhan001 <jalalkhanudemy@gmail.com>
2025-02-13 15:33:32 +0530

    Update sparse_checkout.sh

--- sparse_checkout.sh
+++ sparse_checkout.sh
@@ -3,7 +3,7 @@ project_type=classical      #options: classical / cv / nlp
 mlops_version=aml-cli-v2   #options: aml-cli-v2 / python-sdk-v1 / python-sdk-v2 / rai-aml-cli-v2
 orchestration=azure-devops #options: github-actions / azure-devops
 git_folder_location='<local path>'   #replace with the local root folder location where you want to create the project folder
-project_name=Mlops-Test   #replace with your project name
+project_name=mlopsv2-sparse   #replace with your project name
 github_org_name=orgname   #replace with your github org name
 project_template_github_url=https://github.com/azure/mlops-project-template   #replace with the url for the project template for your organization created in step 2.2, or leave for demo purposes
 
